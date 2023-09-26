fdc74c46c3041715e17f6a9bad8b9c9a21a2e9ca
Alexander <bmgula55@mail.ru>
2023-09-26 23:29:07 +0300

    solution task 1 loading data from a file into a database table

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,24 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees(
+	employee_id int,
+	first_name char(50),
+	last_name char(50),
+	title text,
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers(
+	customer_id char(50),
+	company_name char(50),
+	contact_name char(50)
+);
+
+CREATE TABLE orders(
+	order_id int,
+	customer_id char(50),
+	employee_id int,
+	order_date date,
+	ship_city char(50)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,34 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+from help import host, database, user, password
+
+conn = psycopg2.connect(host=host, database=database, user=user, password=password)
+
+
+def uploading_data_to_the_database(path: str, name_tabel: str):
+    """
+        path - Путь к CSV файлу
+        name_tabel - Название таблицы в БД (куда будут записаны данные из файла CSV "_")
+    """
+
+    with open(path, "r", encoding="UTF-8") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            try:
+                with conn:
+                    with conn.cursor() as cur:
+                        request = f"INSERT INTO {name_tabel} VALUES (" + len(reader.fieldnames) * "%s,"
+                        ready_request = request[:-1] + ")"
+
+                        data = [row[i] for i in reader.fieldnames]
+
+                        cur.execute(ready_request, data)
+
+            except Exception as e:
+                print("Ошибка записи в таблицу БД данных из файла!\n", e)
+
+
+uploading_data_to_the_database("north_data/orders_data.csv", "orders")
+uploading_data_to_the_database("north_data/customers_data.csv", "customers")
+uploading_data_to_the_database("north_data/employees_data.csv", "employees")
